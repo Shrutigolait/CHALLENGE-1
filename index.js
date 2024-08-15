a0baeb59ce8f17b6f375aebd6905e357ef22e787
shruti <shrutigolait576@gmail.com>
2024-08-15 20:35:55 +0530

    index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const massMark=78;
-const heightMark=1.69;
-const massJohn=92;
-const heightJohn=1.91;
+const massMark=78;              //Mark's mass 78Kg
+const heightMark=1.69;          //Mark' height 1.69m
+const massJohn=92;              //John's mass 92kg
+const heightJohn=1.91;          //height 1.91m
 const bmiMark=massMark/(heightMark*heightMark);
 console.log("Mark's Bmi:",bmiMark);
 const bmiJohn=massJohn/(heightJohn*heightJohn);
 console.log("John's Bmi:",bmiJohn);
+const markHigherBMI = bmiMark > bmiJohn;
+console.log("Does Mark have a higher BMI than John?", markHigherBMI); //false
