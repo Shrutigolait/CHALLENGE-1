60b2fa110a7ebd1395564077ecd24fef63bd3d6c
shruti <shrutigolait576@gmail.com>
2024-08-15 20:24:08 +0530

    index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const massMark=78;
+const heightMark=1.69;
+const massJohn=92;
+const heightJohn=1.91;
+const bmiMark=massMark/(heightMark*heightMark);
+console.log("Mark's Bmi:",bmiMark);
+const bmiJohn=massJohn/(heightJohn*heightJohn);
+console.log("John's Bmi:",bmiJohn);
